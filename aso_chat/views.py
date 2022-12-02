265bb93bee054068b88d12328cf1d14c506e6274
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-12-02 19:49:51 +0200

    added chat template

--- aso_chat/views.py
+++ aso_chat/views.py
@@ -23,4 +23,4 @@ def register(request):
 
 def chat_box(request, chat_box_name):
     # we will get the chatbox name from the url
-    return render(request, "chat/chatbox.html", {"chat_box_name": chat_box_name})
+    return render(request, "chat/roomchat.html", {"chat_box_name": chat_box_name})
