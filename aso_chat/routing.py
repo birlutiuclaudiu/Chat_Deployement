d50395e4cf8c0dd125eaecaaea104c5f44fe1e7c
Birlutiu Claudiu <birlutiuclaudiuc@gmail.com>
2022-12-03 02:32:05 +0200

    added dashes option for  websockets

--- aso_chat/routing.py
+++ aso_chat/routing.py
@@ -5,5 +5,5 @@ from aso_chat.consumers import ChatRoomConsumer
 # Here, "" is routing to the URL ChatConsumer which
 # will handle the chat functionality.
 websocket_urlpatterns=[
-                    re_path(r'ws/chat/(?P<chat_box_name>\w+)/$', ChatRoomConsumer.as_asgi()),
+                    re_path(r'ws/chat/(?P<chat_box_name>[\w\-]+)/$', ChatRoomConsumer.as_asgi()),
                 ]
